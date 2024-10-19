8592cc9232fa99b6ab582c3b1f8517cd87a88abc
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-10-18 20:36:51 -0400

    Creates ros2 http-server pkg

---
+++ ros2_ws/src/communications-system/http_server/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'http_server'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Karanpreet Raja',
+    maintainer_email='86526687+KaranpreetRaja@users.noreply.github.com',
+    description='Modular HTTP Communication Server that supports ros2 pub_sub and services',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)

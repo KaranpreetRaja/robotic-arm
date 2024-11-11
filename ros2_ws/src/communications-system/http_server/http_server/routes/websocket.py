498891240a3d84294483ef6e2d3cafae17bbba9f
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-11-11 13:35:04 -0500

    http_server: Adds get_routers() implementation and pre-built routes

---
+++ ros2_ws/src/communications-system/http_server/http_server/routes/websocket.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from std_msgs.msg import String
+import json
+
+router = APIRouter()
+
+def get_node_manager():
+    from http_server.http_server import node_manager
+    return node_manager
+
+@router.websocket("/ws_publish")
+async def websocket_endpoint(websocket: WebSocket):
+    node_manager = get_node_manager()
+    await websocket.accept()
+    node_manager.node.websocket_client = websocket
+    try:
+        while True:
+            data = await websocket.receive_text()
+            message = json.loads(data)
+            topic = message["topic"]
+            message_content = message["message"]
+            
+            if topic and message_content: 
+                if not any(publisher.topic_name == topic for publisher in node_manager.node._publishers):
+                    node_manager.node.add_publisher(topic, String)
+                
+                node_manager.node.publish_message(topic, message_content)
+
+    except WebSocketDisconnect:
+        node_manager.node.websocket_client = None

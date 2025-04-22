ea3c64fdc0aea69d3e412fa7f9b0fb9b42a9fab2
Abdirazak Yusuf <100489227+Abdirazak140@users.noreply.github.com>
2025-04-22 18:40:29 -0400

    Delete ros2_ws/install directory

--- ros2_ws/install/communication_server/lib/python3.10/site-packages/communication_server/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from .admin_api import router as admin_router
-from .websocket import router as websocket_router
-
-def get_routers():
-    router = APIRouter()
-    router.include_router(admin_router, prefix="/admin", tags=["api"])
-    router.include_router(websocket_router, tags=["websocket"])
-    return router
